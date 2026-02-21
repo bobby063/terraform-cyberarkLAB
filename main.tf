7fe424c1ed39b37c4c4fb4c119b6fe8b00b84bdc
Robert Newman <bobby.p.newman@gmail.com>
2026-02-21 10:50:43 +0000

    added safes for simulation

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 locals {
-  # Load safes from YAML file
   safes = yamldecode(file("${path.module}/safes.yaml")).safes
 }
 
 module "safe" {
   source = "./modules/safe"
 
-  # Create one module instance per safe
   for_each = { for s in local.safes : s.name => s }
 
   safe_name   = each.value.name
